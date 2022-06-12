d092e9107ef0b9e6b67833f05f0eb8c82ce77a0c
taghy <adamtaghy@gmail.com>
2022-06-12 20:02:57 +0200

    L

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,69 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
-
+import { Routes, Route, Link } from 'react-router-dom';
+import Form from './Components/Form';
 
 function Home() {
-    return (
-      <main className='App'>
-        <header className='App-header'>
-        <p>
-          restmap
-        </p>
+  return (
+    <main className="App">
+      <header className="App-header">
+        <p>restmap</p>
         <nav>
           <Link to="/about">
-            <button type="button" className='button1'>
+            <button type="button" className="button1">
               About us
             </button>
           </Link>
           <Link to="/program">
-             <button type="button" className='button1'>
+            <button type="button" className="button1">
               Program
-              </button>
-</Link>
+            </button>
+          </Link>
         </nav>
-        </header>
-      </main>
+      </header>
+    </main>
   );
 }
 
 function About() {
-  return (  
-    <main className='App'>
-    <header className='App-header'>
-      <p>
-        yo
-      </p>
-      <Link to="/">
-        <button type="button" className='button1'>
-          Home
-        </button>
-      </Link>
+  return (
+    <main className="App">
+      <header className="App-header">
+        <p>yo</p>
+        <Link to="/">
+          <button type="button" className="button1">
+            Home
+          </button>
+        </Link>
       </header>
-      </main>
+    </main>
   );
 }
 
 function Program() {
-  var form = new NameForm();
-  return (  
-    <main className='App'>
-      <header className='App-header'>
-    <p>
-        content here
-    </p>
-  
-    <Link to="/">
-    <button type="button" className='button1'>
-     Home
-    </button>
-    </Link>
-    </header>
-      </main>
+  return (
+    <main className="App">
+      <header className="App-header">
+        <p>content here</p>
+
+        <Link to="/">
+          <button type="button" className="button1">
+            Home
+          </button>
+        </Link>
+        <Form />
+      </header>
+    </main>
   );
 }
- 
+
 function App() {
   return (
     <div className="App">
@@ -74,37 +67,6 @@ function App() {
       </Routes>
     </div>
   );
-}
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
 }
 
 export default App;
