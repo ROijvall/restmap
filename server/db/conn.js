71baa6efe1ee1cc30a678692b187ff084f9f82bc
ROijvall <rasmus.oijvall.0413@student.uu.se>
2022-07-11 14:20:30 +0200

    added server files and renamed client folder

---
+++ server/db/conn.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+var _db;
+const client = new MongoClient(Db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("restmap");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
